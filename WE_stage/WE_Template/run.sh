eac98cd4eb21803fe70c0a322f72b30fddf491ac
poruthoor <ashlinjamesporuthoor@gmail.com>
2023-05-13 01:29:33 -0400

    Corrected run.sh script

--- WE_stage/WE_Template/run.sh
+++ WE_stage/WE_Template/run.sh
@@ -28,12 +28,4 @@ source env.sh || exit 1
 env | sort
 
 # Run w_run
-# Please note that this script is optimized for the K20Xm GPUs at CIRC, U of R
-# NOTE : You need to change the n-workers parameter with the $NUM_OF_LOCAL_GPUS
-# in a given node. For the K20Xm, it is 2 GPUS per node. This version is better
-# when the work is more GPU dependent than CPUs.
-w_run \
-    --work-manager=processes \
-    --n-workers=2 \
-    "$@" \
-    &> ${LOGDIR}/west-$SLURM_JOBID.log &
+w_run --work-manager processes "$@" &> ${LOGDIR}/west-$SLURM_JOBID.log
