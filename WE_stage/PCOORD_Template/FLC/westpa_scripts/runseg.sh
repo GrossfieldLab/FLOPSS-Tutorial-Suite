e8a3bb30a9fb69bda32f7bd3c38d592f23c8ec4d
poruthoor <ashlinjamesporuthoor@gmail.com>
2023-06-02 15:23:37 -0400

    Removed lipidlist file

--- WE_stage/PCOORD_Template/FLC/westpa_scripts/runseg.sh
+++ WE_stage/PCOORD_Template/FLC/westpa_scripts/runseg.sh
@@ -102,7 +102,6 @@ TEMP_SYSTEM=$(mktemp)
 python3 $WEST_SIM_ROOT/common_files/DBSCANanalysisSystem.py \
     --model $WEST_SIM_ROOT/bstates/model.psf \
     --traj traj.dcd \
-    --lipid_list $WEST_SIM_ROOT/common_files/lipidList.dat \
     --parameter $WEST_SIM_ROOT/common_files/avgRcutoff.dat > $TEMP_SYSTEM || exit 1
 
 cat $TEMP_SYSTEM | tail -n +4 | awk {'print $3'} > $WEST_PCOORD_RETURN
